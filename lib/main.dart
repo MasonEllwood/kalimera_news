9f319413312f06fd5e80b4bdd2437c49a2495846
Mason Ellwood <masonellwood@gmail.com>
2020-09-21 09:02:28 -0700

    add provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:flutterNews/src/pages/kalimera_splash.dart';
 import 'package:flutterNews/src/pages/kalimera_home.dart';
 import 'package:flutterNews/src/transitions/fade_in_transition.dart';
+import 'package:flutterNews/src/provider/kalimera_news_list.dart';
 
 
 void main() => runApp(Kalimera());
@@ -12,21 +13,30 @@ class Kalimera extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'Kalimera App',
-      initialRoute: '/splash',
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/splash':
-            return FadeIn(KalimeraSlash(), settings);
-          case '/home':
-            return FadeIn(KalimeraHome(), settings);
-          case '/':
-          default:
-            return FadeIn(KalimeraHome(), settings);
+    return  withProviders(
+      context,
+      MaterialApp(
+        title: 'Kalimera App',
+        initialRoute: '/splash',
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case '/splash':
+              return FadeIn(KalimeraSlash(), settings);
+            case '/home':
+              return FadeIn(KalimeraHome(), settings);
+            case '/':
+            default:
+              return FadeIn(KalimeraHome(), settings);
+          }
         }
-      }
+      ),
     );
   }
-  
+
+  Widget withProviders(BuildContext context, Widget page) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<KalimeraNewsList>(create: (_) => KalimeraNewsList()),
+    ], child: page);
+  }
+
 }
